f018fb7b6306daa2ad4a42e3b3f3223104ac04b5
Sujoy Paul <freelancersujoy@gmail.com>
2023-10-27 15:46:38 +0600

    pin note

--- src/Pages/Home/PinnedNote/SingleNote.js
+++ src/Pages/Home/PinnedNote/SingleNote.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { toast } from 'react-hot-toast';
-import { BiCopy } from "react-icons/bi";
 import { AiFillEye } from "react-icons/ai";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const SingleNote = ({mynote}) => {
     const {title, note} = mynote;
-    const navigate = useNavigate()
+    // const navigate = useNavigate()
 
     const clickToCopy = () => {
         navigator.clipboard.writeText(mynote?.note)
@@ -15,12 +14,15 @@ const SingleNote = ({mynote}) => {
     }
 
     return (
-        <div className='px-2 py-3 bg-accent mt-2 rounded-xl cursor-pointer hover:rounded-none duration-500'>
-            <div className="flex justify-between items-center">
-                <h4 onClick={clickToCopy} className='text-neutral font-semibold text-md uppercase'>{title ? title.slice(0,20) : note.slice(0,20)}</h4>
+        <div onClick={clickToCopy} className='px-2 py-3 bg-accent mt-2 rounded-xl cursor-pointer duration-500 h-[48px] hover:h-[80px] overflow-hidden'>
+            <div  className="flex justify-between items-center mb-2">
+                <h4  className='text-neutral font-semibold text-md uppercase'>{title ? title.slice(0,20) : note.slice(0,20)}</h4>
                 <Link to={`/note/${mynote._id}`} className='px-2 py-1 h-full'>
-                    <AiFillEye className='text-xl text-neutral animate-pulse duration-500'></AiFillEye>
+                    <AiFillEye className='text-xl text-neutral '></AiFillEye>
                 </Link>
+            </div>
+            <div className=''>
+                <p className='text-sm text-primary'>{note.slice(0,30)}...</p>
             </div>
         </div>
     );
